53c66f728b0172254b2cb28726d4472ff7c64eb3
Andrew McGlynn <andrewmcglynn86@hotmail.com>
2023-07-15 09:47:54 +0100

    Added logging and build and deploy script

--- login-lambda/src/main/java/com/amcglynn/myzappi/login/rest/EndpointRouter.java
+++ login-lambda/src/main/java/com/amcglynn/myzappi/login/rest/EndpointRouter.java
@@ -2,10 +2,12 @@ package com.amcglynn.myzappi.login.rest;
 
 import com.amcglynn.myzappi.core.config.ServiceManager;
 import com.amcglynn.myzappi.login.service.RegistrationService;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class EndpointRouter {
 
     private Map<String, RestController> handlers;
@@ -16,14 +18,18 @@ public class EndpointRouter {
         handlers.put("DELETE /hub", new HubController(new RegistrationService(serviceManager.getLoginService())));
     }
     public Response route(Request request) {
+        if (request.getUserId() == null) {
+            log.info("User not authenticated");
+            return new Response(401);
+        }
         try {
-            System.out.println(request.getMethod()  + " " + request.getPath());
+            log.info("{} {}", request.getMethod(), request.getPath());
             var controller = handlers.get(request.getMethod() + " " + request.getPath());
             if (controller == null) {
-                System.out.println("Controller not found for " + request.getMethod() + " " + request.getPath());
+                log.info("Controller not found for {} {}", request.getMethod(), request.getPath());
                 return new Response(404);
             }
-            System.out.println("Found controller " + controller.getClass());
+            log.info("Found controller {}", controller.getClass());
             return controller.handle(request);
         } catch (ServerException e) {
             return new Response(e.getStatus());
