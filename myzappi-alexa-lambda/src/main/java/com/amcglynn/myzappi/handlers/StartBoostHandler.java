162567f04a3d5472fb9b67173f5c8996a0e68f46
Andrew McGlynn <andrewmcglynn86@hotmail.com>
2024-10-08 21:50:17 +0100

    The boost until duration and end time had flawed assumptions. Removed the ability to boost by endtime or duration on the skill and on the website. Those with schedules already set up for boost by duration or endtime will continue to have this working as it was.

--- myzappi-alexa-lambda/src/main/java/com/amcglynn/myzappi/handlers/StartBoostHandler.java
+++ myzappi-alexa-lambda/src/main/java/com/amcglynn/myzappi/handlers/StartBoostHandler.java
@@ -7,12 +7,9 @@ import com.amazon.ask.request.RequestHelper;
 import com.amcglynn.myenergi.units.KiloWattHour;
 import com.amcglynn.myzappi.UserIdResolverFactory;
 import com.amcglynn.myzappi.UserZoneResolver;
-import com.amcglynn.myzappi.core.Brand;
 import com.amcglynn.myzappi.core.service.MyEnergiService;
 
-import java.time.Duration;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.Optional;
 
@@ -40,69 +37,31 @@ public class StartBoostHandler implements RequestHandler {
 
     @Override
     public Optional<Response> handle(HandlerInput handlerInput) {
-        var duration = parseDurationSlot(handlerInput);
-        var time = parseSlot(handlerInput, "Time");
         var kilowattHours = parseKiloWattHourSlot(handlerInput);
 
         var zappiService = zappiServiceBuilder.build(userIdResolverFactory.newUserIdResolver(handlerInput)).getZappiServiceOrThrow();
-        // The boost API when duration is used needs to have the local time read from the device that made the request.
-        // This is done by reading the base URL and access token from the request and making a request to Alexa API
-        // to retrieve the timezone for the device. Once the timezone is retrieved, it is set here so that when an end
-        // time needs to be calculated, it gets the current local time, adds the duration and then sends that end time
-        // to the myenergi API
 
         var zoneId = userZoneResolver.getZoneId(handlerInput);
         zappiService.setLocalTimeSupplier(() -> LocalTime.now(zoneId));
 
-        if (duration.isPresent()) {
-            return buildResponse(handlerInput, zappiService.startSmartBoost(duration.get()));
-        }
-        if (time.isPresent()) {
-            return buildResponse(handlerInput, zappiService.startSmartBoost(LocalTime.parse(time.get())));
-        }
-        if (kilowattHours.isPresent()) {
-            zappiService.startBoost(kilowattHours.get());
-            return buildResponse(handlerInput, kilowattHours.get());
-        }
-
-        return buildNotFoundResponse(handlerInput);
+        // alexa ensures kilowatt hours slot is populated
+        zappiService.startBoost(kilowattHours.get());
+        return buildResponse(handlerInput, kilowattHours.get());
     }
 
-    private Optional<String> parseSlot(HandlerInput handlerInput, String slotName) {
+    private Optional<String> parseSlot(HandlerInput handlerInput) {
         return RequestHelper.forHandlerInput(handlerInput)
-                .getSlotValue(slotName);
-    }
-
-    private Optional<Duration> parseDurationSlot(HandlerInput handlerInput) {
-        return parseSlot(handlerInput, "Duration").map(Duration::parse);
+                .getSlotValue("KiloWattHours");
     }
 
     private Optional<KiloWattHour> parseKiloWattHourSlot(HandlerInput handlerInput) {
-        return parseSlot(handlerInput, "KiloWattHours").map(Double::parseDouble).map(KiloWattHour::new);
-    }
-
-    private Optional<Response> buildResponse(HandlerInput handlerInput, LocalTime endTime) {
-        return handlerInput.getResponseBuilder()
-                .withSpeech(voiceResponse(handlerInput, "boosting-until-time", Map.of("time",
-                        endTime.format(DateTimeFormatter.ofPattern("h:mm a")))))
-                .withSimpleCard(Brand.NAME, cardResponse(handlerInput, "boosting-until-time", Map.of("time",
-                        endTime.format(DateTimeFormatter.ofPattern("h:mm a")))))
-                .withShouldEndSession(false)
-                .build();
+        return parseSlot(handlerInput).map(Double::parseDouble).map(KiloWattHour::new);
     }
 
     private Optional<Response> buildResponse(HandlerInput handlerInput, KiloWattHour kilowattHours) {
         return handlerInput.getResponseBuilder()
                 .withSpeech(voiceResponse(handlerInput, "boosting-for-kwh", Map.of("kWh", kilowattHours.toString())))
                 .withSimpleCard("My Zappi", cardResponse(handlerInput, "boosting-for-kwh", Map.of("kWh", kilowattHours.toString())))
-                .withShouldEndSession(false)
-                .build();
-    }
-
-    private Optional<Response> buildNotFoundResponse(HandlerInput handlerInput) {
-        return handlerInput.getResponseBuilder()
-                .withSpeech(voiceResponse(handlerInput, "didnt-understand"))
-                .withSimpleCard(Brand.NAME, cardResponse(handlerInput, "didnt-understand"))
                 .withShouldEndSession(false)
                 .build();
     }
